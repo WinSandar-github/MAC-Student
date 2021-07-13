240d8267a52129a8294c9285a8f7e00ace566616
nyeinsandar <nyeinsandar@agga.io>
2021-07-13 18:26:20 +0630

    fixed ui for cpa_ff

--- resources/views/pages/cpa/cpa_ff_register.blade.php
+++ resources/views/pages/cpa/cpa_ff_register.blade.php
@@ -231,10 +231,8 @@
 
                             <div class="row">
                                 <div class="col-md-1"></div>
-                                <label class="col-md-1 col-form-label">{{ __('(က)') }}</label>
-                                <div class="col-md-5 col-form-label">                                        
-                                    လက်မှတ်ရပြည်သူ့စာရင်းကိုင်စာမေးပွဲအောင်လက်မှတ်မူရင်းနှင့်မိတ္တူ
-                                </div>
+                                <label class="col-md-1 col-form-label pt-5">{{ __('(က)') }}</label>
+                                <div class="col-md-5 col-form-label pt-5">လက်မှတ်ရပြည်သူ့စာရင်းကိုင်စာမေးပွဲအောင်လက်မှတ်</div>
                                 <div class="col-md-5">
                                     <div class=" mt-3 mb-3 col-auto">
                                         <div class="single-form">
@@ -246,8 +244,8 @@
 
                             <div class="row">
                                 <div class="col-md-1"></div>
-                                <label class="col-md-1 col-form-label">{{ __('(ခ)') }}</label>
-                                <div class="col-md-5 col-form-label">မြန်မာနိုင်ငံလက်မှတ်ရပြည်သူ့စာရင်းကိုင်များအသင်းဝင်ကတ်ပြား မူရင်းနှင့်မိတ္တူ</div>
+                                <label class="col-md-1 col-form-label pt-5">{{ __('(ခ)') }}</label>
+                                <div class="col-md-5 col-form-label pt-5">မြန်မာနိုင်ငံလက်မှတ်ရပြည်သူ့စာရင်းကိုင်များအသင်းဝင်ကတ်ပြား </div>
                                 <div class="col-md-5">
                                     <div class=" mt-3 mb-3 col-auto"> 
                                         <div class="single-form">
@@ -259,8 +257,21 @@
 
                             <div class="row">
                                 <div class="col-md-1"></div>
-                                <label class="col-md-1 col-form-label">{{ __('(ဂ)') }}</label>
-                                <div class="col-md-5 col-form-label">နိုင်ငံသားစိစစ်ရေးကတ်ပြားအမှတ် မူရင်းနှင့်မိတ္တူ</div>
+                                <label class="col-md-1 col-form-label pt-5">{{ __('(ဂ)') }}</label>
+                                <div class="col-md-5 col-form-label pt-5">နိုင်ငံသားစိစစ်ရေးကတ်ပြား (အရှေ့)</div>
+
+                                <div class="col-md-5">
+                                    <div class=" mt-3 mb-3 col-auto">
+                                        <div class="single-form">
+                                            <input type="file"  class="form-control p-4" name="nrc" required="" style="padding: 2%;">
+                                        </div>                                        
+                                    </div>
+                                </div>
+                            </div><br/>
+                            <div class="row">
+                                <div class="col-md-1"></div>
+                                <label class="col-md-1 col-form-label">{{ __('') }}</label>
+                                <div class="col-md-5 col-form-label pt-5">နိုင်ငံသားစိစစ်ရေးကတ်ပြား (အနောက်)</div>
 
                                 <div class="col-md-5">
                                     <div class=" mt-3 mb-3 col-auto">
@@ -273,8 +284,8 @@
 
                             <div class="row">
                                 <label class="col-md-1 col-form-label"></label>
-                                <label class="col-md-1 col-form-label">{{ __('(ဃ)') }}</label>
-                                <label class="col-md-5 col-form-label">{{ __('စတင်လျှောက်ထားသည့်နေ့မတိုင်မီ ၁၂ လအတွင်း စဥ်ဆက်မပြတ်လေ့လာသင်ယူမှု(Continuous professional Development-CPD)မှတ်တမ်း') }}</label>
+                                <label class="col-md-1 col-form-label pt-5">{{ __('(ဃ)') }}</label>
+                                <label class="col-md-5 col-form-label pt-5">{{ __('စတင်လျှောက်ထားသည့်နေ့မတိုင်မီ ၁၂ လအတွင်း စဥ်ဆက်မပြတ်လေ့လာသင်ယူမှု(Continuous professional Development-CPD)မှတ်တမ်း') }}</label>
                                 <div class="col-md-5">
                                     <div class=" mt-3 mb-3 col-auto">
                                         <div class="single-form">
@@ -286,9 +297,9 @@
 
                             <div class="row">
                                 <div class="col-md-1"></div>
-                                <label class="col-md-1 col-form-label">{{ __('(င)') }}</label>
+                                <label class="col-md-1 col-form-label pt-5">{{ __('(င)') }}</label>
                                 
-                                <div class="col-md-5 col-form-label">ပတ်စပို့အရွယ်ဓာတ်ပုံ</div>
+                                <div class="col-md-5 col-form-label pt-5">ပတ်စပို့အရွယ်ဓာတ်ပုံ</div>
                                 <div class="col-md-5">
                                     <div class=" mt-3 mb-3 col-auto">
                                         <div class="single-form">
