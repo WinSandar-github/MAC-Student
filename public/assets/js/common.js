72c8c0ee4221fb8859ea561a2f0a015b2ca908fe
wuttyee <wuttyee@agga.io>
2021-07-22 16:11:51 +0630

    fix add frontend url

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -1,5 +1,6 @@
 // var BACKEND_URL="http://localhost:8000/api";
 var BACKEND_URL="https://demo.aggademo.me/MAC/public/index.php/api";
+var FRONTEND_URL="https://demo.aggademo.me/MAC_Student/public/index.php";
 var toastOptions = {
     "closeButton": true,
     "debug": false,
