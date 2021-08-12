a7b322523bbe35999aaf9bcb5f1e7c603ca93282
nyeinsandar <nyeinsandar@agga.io>
2021-08-12 19:55:07 +0630

    fixed ui

--- resources/views/pages/student_course.blade.php
+++ resources/views/pages/student_course.blade.php
@@ -41,7 +41,7 @@
             <img class="shape-author" src="{{ asset('assets/images/author/author-11.jpg')}}" alt="Shape">
         </div>
         <div class="section section-padding mt-n10">
-            <div class="container">
+            <div class="container-fluid p-4">
                 <div class="status-reject" style="display:none">
                     <div class="card text-white bg-dark mb-3">
                         
@@ -72,60 +72,93 @@
                      
                          @php ++$i;  @endphp
                         <input type="hidden" value="{{$c['id']}}" id="course_id{!! $i !!}"/>
-                            <div class="card col-md-5 col-lg-5 offset-md-1 ">
-                                <div class="card-header">
+                            <div class="card col-md-12 mb-3">
+                                <!-- <div class="card-header">
                                     <h4 class="card-title text-center">{{ $c['name']}} </h4>
-                                </div>
+                                </div> -->
                                 <div class="card-body">
-                                <div class="mt-2 widget-information mb-2">
-                                            <div class="info-price">
-                                                <span class="price">{{$c['registration_fee']}} Kyats</span>
-                                            </div>
-                                            <div class="info-list">
-                                                <ul>
-                                                    <li><i class="icofont-man-in-glasses"></i> <strong>Registration Fee</strong> <span>{{$c['registration_fee']}} Kyats</span></li>
+                                    <div class="row">
+                                        <div class="col-md-12 widget-information" >
+                                            <div class="row border-bottom">
+                                                <h3 class="col-md-8 card-title text-center text-success">{{ $c['name']}} </h3>
+                                                @if(!empty($c['active_batch']))
                                                     
-                                                    <li><i class="icofont-man-in-glasses"></i> <strong>Form Fee</strong> <span>{{$c['form_fee']}} Kyats</span></li>
-                                                    <li><i class="icofont-man-in-glasses"></i> <strong>Exam Fee</strong> <span>{{$c['exam_fee']}} Kyats</span></li>
-                                                    <li><i class="icofont-man-in-glasses"></i> <strong>Tution Fee</strong> <span>{{$c['tution_fee']}} Kyats</span></li>
-
-                                                    {{--<li><i class="icofont-clock-time"></i> <strong>Registration End</strong> <span>{{$c['registration_end_date']}}</span></li>--}}
+                                                    @foreach($c['active_batch'] as $b)
+                                                        <!-- <input type="input" value="{{$b['accept_application_start_date']}}"/> -->
+                                                        <input type="hidden" value="{{$b['id']}}" id="batch_id{!! $i !!}"/>
+                                                        <div class="info-btn mb-4 col-md-4 check_login{!! $i !!}">
+                                                        
+                                                        <!-- $c['id'], 'da_register' -->
+                                                        <!-- sucess da         -->
+                                                        <!-- <a href="{{ url('cpa_register') }}" class="btn btn-primary btn-hover-dark d-none " >Enroll Now </a> -->
                                                     
-                                                    <li><i class="icofont-certificate-alt-1"></i> <strong>Certificate</strong> <span>Yes</span></li>
-                                                </ul>
+                                                        <!-- <a href="{{ url('login') }}" class="btn btn-primary btn-hover-dark  d-none  logined" >Enroll Now </a> -->
+
+                                                    
+                                                        </div>
+                                                        @break
+                                                        
+                                                    
+                                                    @endforeach
+                                                @else
+                                                <p class="info-btn col-md-4 mb-4 text-dark h6 ">Coming Soon...</p>
+
+                                                @endif
                                             </div>
-                                        
-                                    </div>
-                                    <div class=" widget-information" >
-                                        
-                                        <h6>Description</h6>
-                                        <hr>
+                                            <div class="row">
+                                                <div class="col-md-8">
+                                                    <h5 class="mt-2">Requirement</h5>
+                                                    <hr>
 
-                                    
-                                        <p style="height:150px;overflow:auto;">{{$c['description']}}</p>
+                                                    <ul class="requirement_lists">
 
-                                    </div>
-                                    @if(!empty($c['active_batch']))
-                                     @foreach($c['active_batch'] as $b)
-                                        <input type="hidden" value="{{$b['id']}}" id="batch_id{!! $i !!}"/>
-                                      <div class="info-btn mt-2 text-center check_login{!! $i !!}">
-                                          
-                                        <!-- $c['id'], 'da_register' -->
-                                       <!-- sucess da         -->
-                                        <!-- <a href="{{ url('cpa_register') }}" class="btn btn-primary btn-hover-dark d-none " >Enroll Now </a> -->
-                                       
-                                        <!-- <a href="{{ url('login') }}" class="btn btn-primary btn-hover-dark  d-none  logined" >Enroll Now </a> -->
+                                                    </ul>
+                                                    <!-- <p style="height:150px;overflow:auto;">{{$c['description']}}</p> -->
+                                                </div>
+                                                <div class="col-md-4">
+                                                    <!-- <div class="info-price">
+                                                        <span class="price">{{$c['registration_fee']}} Kyats</span>
+                                                    </div> -->
+                                                    <div class="info-list">
+                                                        @foreach($c['active_batch'] as $b)
+                                                            <label for="" class="bordered">{{$b['accept_application_start_date']}}</label>
+                                                            <!-- <input type="hidden" value="{{$b['id']}}" id="batch_id{!! $i !!}"/>
+                                                            <div class="info-btn mb-4 col-md-4 check_login{!! $i !!}"> -->
+                                                            
+                                                            
 
-                                    
+                                                        
+                                                            <!-- </div> -->
+                                                            @break
+                                                           
+                                                        
+                                                        @endforeach
+                                                        <ul>
+                                                            <li><i class="icofont-man-in-glasses"></i> <strong>Registration Fee</strong> <span>{{$c['registration_fee']}} Kyats</span></li>
+                                                            
+                                                            <li><i class="icofont-man-in-glasses"></i> <strong>Form Fee</strong> <span>{{$c['form_fee']}} Kyats</span></li>
+                                                            <li><i class="icofont-man-in-glasses"></i> <strong>Exam Fee</strong> <span>{{$c['exam_fee']}} Kyats</span></li>
+                                                            <li><i class="icofont-man-in-glasses"></i> <strong>Tution Fee</strong> <span>{{$c['tution_fee']}} Kyats</span></li>
+
+                                                            {{--<li><i class="icofont-clock-time"></i> <strong>Registration End</strong> <span>{{$c['registration_end_date']}}</span></li>--}}
+                                                            
+                                                            <li><i class="icofont-certificate-alt-1"></i> <strong>Certificate</strong> <span>Yes</span></li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            
+                                            
+
+                                        <!-- </div>
+                                        <div class="col-md-6 pr-2 widget-information"> -->
+                                            
+                                            
                                         </div>
-                                        @break
                                         
+                                    </div>
                                     
-                                     @endforeach
-                                    @else
-                                    <p class="text-center text-dark mt-2 h6 ">Coming Soon</p>
-
-                                    @endif
+                                    
                                 
                                     
                                 </div>
@@ -158,9 +191,31 @@
 <script type="text/javascript">
     $('document').ready(function(){
         var course_type = location.pathname.split('/');
+        // console.log('course_type',course_type[2]);
         var student = JSON.parse(localStorage.getItem('studentinfo'));
         if(!student){
         localStorage.setItem('course_type',course_type[2])
+        }
+        if(course_type[2]==1){
+            // console.log("DA");
+            var li = "<li class='mb-2'> <i class='fa fa-check'></i>အသိအမှတ်ပြုတက္ကသိုလ်တစ်ခုခုမှ ဘွဲ့ရရှိသူများ လျှောက်ထားနိုင်ပါသည်။ </li>";
+            li += "<li class='mb-2'>  <i class='fa fa-check'></i>DA I  သင်တန်း (MAC,SS,Private)(၃)မျိုးပေါင်းနှစ်စဉ် ၅၀၀ ဦးခန့်ရှိ။</li>";
+            li += "<li class='mb-2'>  <i class='fa fa-check'></i>DA II သင်တန်း(၂)တန်း (MAC,SS,Private)(၃)မျိုးပေါင်း နှစ်စဉ် ၃၀၀  ဦးခန့်ရှိ။</li>";
+            li += "<li class='mb-2'>  <i class='fa fa-check'></i>Module အားလုံးကိုဖြစ်စေ၊ ကြိုက်နှစ်သက်ရာ Module တစ်ခုကိုဖြစ်စေ ၅ နှစ်အတွင်းဖြေဆိုနိုင်သည်။</li>";
+            li += "<li class='mb-2'>  <i class='fa fa-check'></i>DA I သင်တန်းတွင် ဘာသာရပ် ၅ ခု၊ Module I တွင် ဘာသာရပ် ၃ ခု၊ Module II တွင် ဘာသာရပ် ၂ ခု ရှိပါသည်။</li>";
+            li += "<li class='mb-2'>  <i class='fa fa-check'></i>DA II သင်တန်းတွင် ဘာသာရပ် ၆ ခုရှိပြီး ရှိပြီး Module တစ်ခုစီတွင် ဘာသာရပ် ၃ ခုရှိပါသည်။</li>";
+            li += "<li class='mb-2'>  <i class='fa fa-check'></i>ယနေ့ထိ DA I အောင်မြင်ပြီးသူ  ၈၇၄၃ ဦး၊ တက်ရောက်ဆဲ ၂၇၉ ဦး ရှိပါသည်။</li>";
+            li += "<li class='mb-2'>  <i class='fa fa-check'></i>ယနေ့ထိ DA II အောင်မြင်ပြီးသူ  ၄၆၀၉ ဦး ရှိပါသည်။</li>";
+            $(".requirement_lists").append(li);
+        }else{
+            // console.log("CPA");
+            var li = "<li class='mb-2'> <i class='fa fa-check'></i>BCom,BAct,BBA,DA,BBSc ,ACCA (Fundamental skill level),CIMA ဘွဲ့များကို အဆိုပါသင်တန်းသို့ တိုက်ရိုက် တက်ရောက်ခွင့်ပေးပြီး အခြားဘွဲ့များ ဝင်ခွင့်စာမေးပွဲ အောင်မြင်ပါက တက်ရောက်ခွင့်ရှိပါသည်။</li>";
+            li += "<li class='mb-2'>  <i class='fa fa-check'></i>CPA I & II သင်တန်း(၂)တန်းဖွင့်လှစ်ပြီး (MAC,SS,Private)(၃)မျိုးပေါင်းနှစ်စဉ် ၃၅၀၀ ဦးခန့်ရှိ။</li>";
+            li += "<li class='mb-2'>  <i class='fa fa-check'></i>Module အားလုံးကိုဖြစ်စေ၊ ကြိုက်နှစ်သက်ရာ Module တစ်ခုကိုဖြစ်စေ ၅ နှစ်အတွင်းဖြေဆိုနိုင်သည်။</li>";
+            li += "<li class='mb-2'>  <i class='fa fa-check'></i>သင်တန်းတစ်ခုတွင်ဘာသာရပ် ၆ ခုရှိပြီး Module တစ်ခုတွင် ဘာသာရပ် ၃ ခုရှိပါသည်။</li>";
+            li += "<li class='mb-2'>  <i class='fa fa-check'></i>ယနေ့ထိ CPA I အောင်မြင်ပြီးသူ  ၃၈၁၀ ဦး၊တက်ရောက်ဆဲ၂၇၄၁ ဦး တက်ရောက်ခွင့်ရရှိထားသူ ၁၅၀၀ ဦး ခန့်ရှိပါသည်။</li>";
+            li += "<li class='mb-2'>  <i class='fa fa-check'></i>CPA II  အောင်မြင်ပြီးသူ ၂၆၂၈ ဦး ၊ CPA II တက်ရောက်ဆဲ    ၅၀၀ ဦး ရှိပါသည်။</li>";
+            $(".requirement_lists").append(li);
         }
     })
     app_form_feedback();
