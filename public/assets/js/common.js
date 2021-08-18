6b51616505d9ad9e3872efc3004930dc6a882399
Si Thu <sithuaung182@gmail.com>
2021-08-18 11:35:43 +0630

    show login error message and add base url

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -1,10 +1,10 @@
-var BACKEND_URL="http://localhost:8000/api";
-var FRONTEND_URL="http://localhost:8001";
-var BASE_URL = "http://localhost:8000";
+// var BACKEND_URL="http://localhost:8000/api";
+// var FRONTEND_URL="http://localhost:8001";
+// var BASE_URL = "http://localhost:8000";
 
-// var BACKEND_URL="https://demo.aggademo.me/MAC/public/index.php/api";
-// var FRONTEND_URL = "https://demo.aggademo.me/MAC_Student/public/index.php";
-// var BASE_URL = "https://demo.aggademo.me/MAC_Student/public/index.php";
+var BACKEND_URL="https://demo.aggademo.me/MAC/public/index.php/api";
+var FRONTEND_URL = "https://demo.aggademo.me/MAC_Student/public/index.php";
+var BASE_URL = "https://demo.aggademo.me/MAC_Student/public/index.php";
 
 var counter = 0;
 
@@ -54,16 +54,6 @@ function formatDate(date){
     var income_date=date.split('-');
     var date=income_date[2]+'-'+income_date[1]+'-'+income_date[0];
     return date;
-}
-
-function ConfirmSubmit(){
-    var radio = document.getElementById("submit_confirm");
-    if (radio.checked == true){
-        document.getElementById("submit_btn").disabled= false;
-    } 
-    else{
-    document.getElementById("submit_btn").disabled = true;
-    }
 }
 
 function addRowEducation(tbody){
