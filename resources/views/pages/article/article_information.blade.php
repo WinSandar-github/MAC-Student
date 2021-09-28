b843e67fd02b54a21d762bfc43c11abb14f70ec3
Aung Hein Soe <heinsoe@agga.io>
2021-09-28 06:44:10 +0000

    Updated article_information.blade.php

--- resources/views/pages/article/article_information.blade.php
+++ resources/views/pages/article/article_information.blade.php
@@ -204,7 +204,7 @@
                     let student_reg = data.data.student_register
                     let article = data.data.article;
                     let lastest_row = student_reg.length - 1;
-                    let course = student_reg[lastest_row].course.code;  // cpa1/cpa2
+                    let course = student_reg[lastest_row]?.course?.code;  // cpa1/cpa2
                     let exam_result = student_reg[lastest_row].status;  // pass/fail
                     let module = student_reg[lastest_row].module;  // module 1/2/all
                     let type = student_reg[lastest_row].type;  //  0-self_study / 1-private / 2-mac
