a7beaa533643ed932e5e62d279eca6667c1d9d24
Kaung Cho <admin@agga.io>
2021-10-01 10:51:54 +0630

    fix again

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -79,10 +79,10 @@
 
         <!-- Slider Video Start -->
         <div class="slider-video">
-            <img class="shape-1" src="assets/images/shape/shape-9.png" alt="Shape">
+            {{-- <img class="shape-1" src="assets/images/shape/shape-9.png" alt="Shape"> --}}
 
             <div class="video-play">
-                <img src="assets/images/shape/shape-10.png" alt="Shape">
+                {{-- <img src="assets/images/shape/shape-10.png" alt="Shape"> --}}
                 <a href="https://www.youtube.com/watch?v=BRvyWfuxGuU" class="play video-popup"><i class="flaticon-play"></i></a>
             </div>
         </div>
