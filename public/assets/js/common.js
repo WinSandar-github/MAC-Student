bbd9eda23adeb77168da8a3ebc6ebedfce827618
hein soe <heinsoe@agga.io>
2021-10-09 20:55:11 +0630

    add backendService url js file

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -2,9 +2,9 @@
 // var BASE_URL = "http://localhost:8000";
 // var BACKEND_URL = "http://localhost:8000/api";
 
-var BACKEND_URL = "https://demo.aggademo.me/MAC/public/index.php/api";
-var FRONTEND_URL = "https://demo.aggademo.me/MAC_Student/public/index.php";
-var BASE_URL = "https://demo.aggademo.me/MAC/public/";
+// var BACKEND_URL = "https://demo.aggademo.me/MAC/public/index.php/api";
+// var FRONTEND_URL = "https://demo.aggademo.me/MAC_Student/public/index.php";
+// var BASE_URL = "https://demo.aggademo.me/MAC/public/";
 
 var counter = 0;
 
