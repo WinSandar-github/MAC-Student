2a070d6c95c53869afee0e549d6fc8bd7e7a7fbf
sandar <sandar@agga.io>
2021-10-11 17:24:45 +0630

    adding teacher

--- public/assets/js/backendService.js
+++ public/assets/js/backendService.js
@@ -3,6 +3,6 @@
 // const BACKEND_URL = "http://localhost:8000/api";
 
 
-// const FRONTEND_URL = "https://demo.aggademo.me/MAC_Student/public/index.php";
-// const BACKEND_URL = "https://demo.aggademo.me/MAC/public/index.php/api";
-// const BASE_URL = "https://demo.aggademo.me/MAC/public/";
+const FRONTEND_URL = "https://demo.aggademo.me/MAC_Student/public/index.php";
+const BACKEND_URL = "https://demo.aggademo.me/MAC/public/index.php/api";
+const BASE_URL = "https://demo.aggademo.me/MAC/public/";
