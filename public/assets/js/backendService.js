c094e6099001060474e655a7fabd28d55d58def7
Naw Lay Wah Shee <laywahshee@agga.io>
2021-10-13 13:37:17 +0630

    papp

--- public/assets/js/backendService.js
+++ public/assets/js/backendService.js
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
 // const FRONTEND_URL = "http://localhost:8001";
 // const BASE_URL = "http://localhost:8000";
 // const BACKEND_URL = "http://localhost:8000/api";
 
-=======
-// const FRONTEND_URL = "http://localhost:8081";
-// const BASE_URL = "http://localhost:8000";
-// const BACKEND_URL = "http://localhost:8000/api";
->>>>>>> de0446a50523ad018ebda925372ab73f80becc36
 
 const FRONTEND_URL = "https://demo.aggademo.me/MAC_Student/public/index.php";
 const BACKEND_URL = "https://demo.aggademo.me/MAC/public/index.php/api";
