a2176076e4ffead3cbc51757beb285ae94e73765
sandar <sandar@agga.io>
2021-10-13 15:27:09 +0630

    update teacher and school

--- resources/views/pages/home/dashboard.blade.php
+++ resources/views/pages/home/dashboard.blade.php
@@ -571,86 +571,87 @@
                                         <span id="cpaff_nrc"></span>
                                     </div>
                                 </div>
-
-
-                </div>--}}
-                <div class="card-body cpaff_other">
-                        <div class="row">
-                            <div class="col-md-3">
-                                <img class="img-fluid" id="cpaff_image" style="height : 200px;width:auto;"/>
                             </div>
-                            <div class="col-md-6">
-                                <div class="row m-2 mt-3 border-bottom">
-                                    <div class="col-md-6">
-                                        <p class="ml-2 text-bold" style="font-weight:bold">Name(Eng) / Name(Myanmar)</p>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <span id="cpaff_name_eng"></span> / <span id="cpaff_name_mm"></span>
-                                    </div>
-                                </div>
-                                <div class="row m-2 mt-3 border-bottom">
-                                    <div class="col-md-6">
-                                        <p class="ml-2" style="font-weight:bold">NRC</p>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <span id="cpaff_nrc"></span>
-                                    </div>
-                                </div>
-
-                                <div class="row m-2 mt-3 border-bottom">
-                                    <div class="col-md-6">
-                                        <p class="ml-2" style="font-weight:bold">Email</p>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <span id="cpaff_email"></span>
-                                    </div>
-                                </div>
-                                <div class="row m-2 mt-3 border-bottom">
-                                    <div class="col-md-6">
-                                        <p class="ml-2" style="font-weight:bold">Phone</p>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <span id="cpaff_phone"></span>
-                                    </div>
-                                </div>
-                                <div class="row m-2 mt-3 border-bottom period" style="display:none;">
-                                    <div class="col-md-6">
-                                        <p class="ml-2" style="font-weight:bold">This certificate is valid for the
-                                            period</p>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <span id="period_time_cpaff"></span>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div class="col-md-3">
-                                <div class="row m-2 mt-3 border-bottom">
-                                    <div class="col-md-10 offset-md-1 mb-2">
-                                        <button type="submit" class="btn btn-success btn-sm btn-hover-dark w-100"
-                                            data-bs-toggle="modal"
-                                            data-bs-target="#showPwdModel">{{ __('Change Password') }}</button>
-                                    </div>
-                                </div>
-                                <div class="row m-2 mt-3 border-bottom">
-                                    <div class="col-md-10 offset-md-1 mb-2">
-                                        <button type="submit" class="btn btn-success btn-sm btn-hover-dark w-100"
-                                            onclick="Cpaff_profile_update()">{{ __('Update Profile') }}</button>
-                                    </div>
-                                </div>
-                            </div>
-
                         </div>
+                    </div>--}}
+                    <div class="card-body cpaff_other">
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <img class="img-fluid" id="cpaff_image" style="height : 200px;width:auto;"/>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="row m-2 mt-3 border-bottom">
+                                        <div class="col-md-6">
+                                            <p class="ml-2 text-bold" style="font-weight:bold">Name(Eng) / Name(Myanmar)</p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <span id="cpaff_name_eng"></span> / <span id="cpaff_name_mm"></span>
+                                        </div>
+                                    </div>
+                                    <div class="row m-2 mt-3 border-bottom">
+                                        <div class="col-md-6">
+                                            <p class="ml-2" style="font-weight:bold">NRC</p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <span id="cpaff_nrc"></span>
+                                        </div>
+                                    </div>
 
-                        <div class="row my-2">
-                            <h5 class="text-center border-bottom pb-2" style="font-weight:bold ;background-color:#E7F8EE;">
-                                    Application Status</h5>
-                            <div class="text-center status_history"></div>
-                            <div class="text-center status_papp"> </div>
-                        </div>
+                                    <div class="row m-2 mt-3 border-bottom">
+                                        <div class="col-md-6">
+                                            <p class="ml-2" style="font-weight:bold">Email</p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <span id="cpaff_email"></span>
+                                        </div>
+                                    </div>
+                                    <div class="row m-2 mt-3 border-bottom">
+                                        <div class="col-md-6">
+                                            <p class="ml-2" style="font-weight:bold">Phone</p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <span id="cpaff_phone"></span>
+                                        </div>
+                                    </div>
+                                    <div class="row m-2 mt-3 border-bottom period" style="display:none;">
+                                        <div class="col-md-6">
+                                            <p class="ml-2" style="font-weight:bold">This certificate is valid for the
+                                                period</p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <span id="period_time_cpaff"></span>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="row m-2 mt-3 border-bottom">
+                                        <div class="col-md-10 offset-md-1 mb-2">
+                                            <button type="submit" class="btn btn-success btn-sm btn-hover-dark w-100"
+                                                data-bs-toggle="modal"
+                                                data-bs-target="#showPwdModel">{{ __('Change Password') }}</button>
+                                        </div>
+                                    </div>
+                                    <div class="row m-2 mt-3 border-bottom">
+                                        <div class="col-md-10 offset-md-1 mb-2">
+                                            <button type="submit" class="btn btn-success btn-sm btn-hover-dark w-100"
+                                                onclick="Cpaff_profile_update()">{{ __('Update Profile') }}</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div class="row my-2">
+                                <h5 class="text-center border-bottom pb-2" style="font-weight:bold ;background-color:#E7F8EE;">
+                                        Application Status</h5>
+                                <div class="text-center status_history"></div>
+                                <div class="text-center status_papp"> </div>
+                            </div>
+                        
                     </div>
+                </div>
             </div>
-
             <!-- cpaff other dashboard -->
             {{--<div class="cpaff_other">
                 <div class="card">
