689e1def7cba18b7b82ab75d13d6641a1341cb20
sandar <sandar@agga.io>
2021-10-13 15:04:12 +0630

    update teacher and school

--- public/assets/js/school.js
+++ public/assets/js/school.js
@@ -257,7 +257,48 @@ function school_reg_feedback(){
 }
 
 function getCourses(){
-  
+  var numeralCodes = [
+    {
+      num: '1',			
+      numeral : 'I',
+    },
+    {
+      num: '2',			
+      numeral : 'II',
+    },
+    {
+      num: '3',			
+      numeral : 'III',
+    },
+    {
+      num: '4',			
+      numeral : 'IV',
+    },
+    {
+      num: '5',			
+      numeral : 'V',
+    },
+    {
+      num: '6',			
+      numeral : 'VI',
+    },
+    {
+      num: '7',			
+      numeral : 'VII',
+    },
+    {
+      num: '8',			
+      numeral : 'VIII',
+    },
+    {
+      num: '9',			
+      numeral : 'IX',
+    },
+    {
+      num: '10',			
+      numeral : 'X',
+    },
+  ];
   $.ajax({
       url:BACKEND_URL+'/get_courses',
       type:'get',
@@ -266,6 +307,7 @@ function getCourses(){
           $.each(response.data,function(i,v){
               var newcode=(v.code).split('_');
               var result = numeralCodes.filter( obj => obj.num === newcode[1])[0];
+              console.log(result+":"+result.numeral);
               var course_code=result.numeral;
               
               opt += `<option value=${v.id}  >${newcode[0].toUpperCase()+' '+course_code}</option>`;
