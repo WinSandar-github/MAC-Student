961debe01c3d1690019901d6fde071c225862ddf
sandar <sandar@agga.io>
2021-10-17 01:48:53 +0630

    update teacher and school

--- public/assets/js/backendService.js
+++ public/assets/js/backendService.js
@@ -1,6 +1,6 @@
 // const FRONTEND_URL = "http://localhost:8001";
-// const BASE_URL = "http://localhost:8080";
-// const BACKEND_URL = "http://localhost:8080/api";
+// const BASE_URL = "http://localhost:8000";
+// const BACKEND_URL = "http://localhost:8000/api";
 
 const FRONTEND_URL = "https://demo.aggademo.me/MAC_Student/public/index.php";
 const BACKEND_URL = "https://demo.aggademo.me/MAC/public/index.php/api";
