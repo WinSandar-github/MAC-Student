1b5abce9c80d25eb2708a1a67b41c388d00398b1
Naw Lay Wah Shee <laywahshee@agga.io>
2021-10-17 15:51:41 +0630

    papp

--- public/assets/js/user_profile.js
+++ public/assets/js/user_profile.js
@@ -151,10 +151,19 @@ function user_profile() {
                     if (papp_latest_data.type == 0) {
                         is_renew = "Initial";
                     }
-                    else {
-                        is_renew = "Renewal"
+                    else if (papp_latest_data.type == 1) {
+                        is_renew = "Renewal";
                     }
-                    if (papp_latest_data.status == 0) {
+                    else {
+                        is_renew = ""
+                    }
+                    if(papp_latest_data.status == 0 && papp_latest_data.type == 2)
+                    {
+                        $('.status_history').append('PAPP ' + is_renew + ' Registration Form is Approved.<br><br>');
+                        $('.status_history').append('Action &nbsp;&nbsp;');
+                        $('.status_history').append(`<a href= ${papp_renew_url} class="btn btn-success btn-sm xl-auto" > PAPP Renew Form </a><hr>`);
+                    }
+                    else if (papp_latest_data.status == 0) {
                         $('.status_history').append('PAPP ' + is_renew + ' Registration Form is checking.<br><br>');
                         $('.status_papp').css('display', 'none');
                     } else if (papp_latest_data.status == 1) {
