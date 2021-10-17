715ac8bce79ab239035e38ed436932cbcdcb67f2
Naw Lay Wah Shee <laywahshee@agga.io>
2021-10-18 05:43:20 +0630

    papp

--- public/assets/js/navbar.js
+++ public/assets/js/navbar.js
@@ -56,7 +56,7 @@ $(document).ready(function () {
                               <li><a href="${FRONTEND_URL}/non_audit_firm_information">Non Audit Firm</a></li>
                             </ul>
                         </li>
-
+                        <li><a href="${FRONTEND_URL}/reconnect">Existing User Register</a></li>
                         <li><a href="${FRONTEND_URL}/contact">Contact</a></li>`
         }
     } else {
@@ -86,6 +86,7 @@ $(document).ready(function () {
                             <li><a href="${FRONTEND_URL}/non_audit_firm_information">Non Audit Firm</a></li>
                         </ul>
                     </li>
+                    <li><a href="${FRONTEND_URL}/reconnect">Existing User Register</a></li>
                     <li><a href="${FRONTEND_URL}/contact">Contact</a></li>`
     }
     $('#navbar').append(navbar);
