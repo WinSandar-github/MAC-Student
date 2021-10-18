8a1e5ec79b94f664e9a19d9e912a808b712d2d55
Twe Tar <twetar@agga.io>
2021-10-18 03:09:31 +0000

    update server

--- app/Http/Controllers/CustomClass/Helper.php
+++ app/Http/Controllers/CustomClass/Helper.php
@@ -5,16 +5,6 @@ use Illuminate\Http\Request;
 
 class Helper
 {
-<<<<<<< HEAD
    public static $domain = 'https://demo.aggademo.me/MAC/public/index.php/api';
    public static $BASE_URL = 'https://demo.aggademo.me/MAC/public/index.php';
-=======
-    public static $domain = 'https://demo.aggademo.me/MAC/public/index.php/api';
-    public static $BASE_URL = 'https://demo.aggademo.me/MAC/public/index.php';
-
-    // public static $domain="http://localhost:8000/api";
-    // public static $BASE_URL="http://localhost:8000";
-
-
->>>>>>> 51c867e083f89d7b4a2ca778d9ad1728cdd84833
 }
