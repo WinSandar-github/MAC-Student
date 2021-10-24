c1b8ea7b161fcfb1b0e7d2373e7cdb67b34a5e15
nyeinsandar <nyeinsandar@agga.io>
2021-10-25 00:14:23 +0630

    reject da/cpa existing user

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -1,10 +1,10 @@
-// const FRONTEND_URL = "http://localhost:8001";
-// const BASE_URL = "http://localhost:8000";
-// const BACKEND_URL = "http://localhost:8000/api";
+const FRONTEND_URL = "http://localhost:8081";
+const BASE_URL = "http://localhost:8000";
+const BACKEND_URL = "http://localhost:8000/api";
 
-const FRONTEND_URL = "https://demo.aggademo.me/MAC_Student/public/index.php";
-const BACKEND_URL = "https://demo.aggademo.me/MAC/public/index.php/api";
-const BASE_URL = "https://demo.aggademo.me/MAC/public/";
+// const FRONTEND_URL = "https://demo.aggademo.me/MAC_Student/public/index.php";
+// const BACKEND_URL = "https://demo.aggademo.me/MAC/public/index.php/api";
+// const BASE_URL = "https://demo.aggademo.me/MAC/public/";
 
 
 var counter = 0;
